dedc85e2b7604530c71360ecc30dbb45749c3dea
Roman <roman@optimistdigital.com>
2022-01-03 00:19:32 +0200

    added similar posts section, in single post page

--- src/sections/SimilarPosts/index.jsx
+++ src/sections/SimilarPosts/index.jsx
@@ -1,15 +1,32 @@
 import React from "react";
+import {Card} from "../../components";
+
+import card_5 from '../../img/cards/card_5.jpg'
+import card_4 from '../../img/cards/card_4.jpg'
+import card_3 from '../../img/cards/card_3.jpg'
 
 const SimilarPosts = (props) => {
-    const { } = props;
-  
     const {className} = props
+
     const classes = 'SimilarPosts ' + (className || '');
 
     return (
-      <div className={classes}>
-        
-      </div>
+      <section className='SimilarPosts__section'>
+          <div className="container">
+              <ul className={classes}>
+                  <li>
+                      <Card img={card_5} className='SimilarPosts__Card'/>
+                  </li>
+                  <li>
+                      <Card img={card_4} className='SimilarPosts__Card'/>
+                  </li>
+                  <li>
+                      <Card img={card_3} className='SimilarPosts__Card'/>
+                  </li>
+              </ul>
+          </div>
+      </section>
+
     );
 
 };
